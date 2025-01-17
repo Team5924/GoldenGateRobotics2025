320390c3fa073c7f596eee14a5bde01b0f4b0efc
Jack Doherty <jack.doherty@oracle.com>
2025-01-17 00:22:20 -0800

    added comments and default message

--- src/main/java/org/team5924/frc2025/BuildConstants.java
+++ src/main/java/org/team5924/frc2025/BuildConstants.java
@@ -21,13 +21,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "GoldenGateRobotics2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 10;
-  public static final String GIT_SHA = "e9f60b6d7e9ae39a9033f3c79c4ba924fea7c66b";
-  public static final String GIT_DATE = "2025-01-17 00:03:21 EST";
+  public static final int GIT_REVISION = 11;
+  public static final String GIT_SHA = "ee15a86734c08878666d719e0cec94b21316d7db";
+  public static final String GIT_DATE = "2025-01-17 00:05:12 EST";
   public static final String GIT_BRANCH = "8-mechanical-runtime-exception";
-  public static final String BUILD_DATE = "2025-01-17 00:04:48 EST";
-  public static final long BUILD_UNIX_TIME = 1737090288966L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2025-01-17 03:21:33 EST";
+  public static final long BUILD_UNIX_TIME = 1737102093037L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
--- src/main/java/org/team5924/frc2025/util/exceptions/MechanicalRuntimeException.java
+++ src/main/java/org/team5924/frc2025/util/exceptions/MechanicalRuntimeException.java
@@ -16,17 +16,45 @@
 
 package org.team5924.frc2025.util.exceptions;
 
+/**
+ * A {@code MechanicalRuntimeException} extends {@code} RuntimeException} and is thrown if the robot
+ * physically enters an error state. This can be used for minor errors or catastrophic errors.
+ *
+ * <p>{@code MechanicalRuntimeException} and its subclasses are <em>unchecked exceptions</em>. For
+ * our purposes, this means that if the exception is not meant to be fatal, it should be handled
+ * wihin a try/catch block.
+ */
 public class MechanicalRuntimeException extends RuntimeException {
-  private static final String DEFAULT_MSG = "default msg";
+  private static final String DEFAULT_MSG =
+      """
+        An error state exists on the physical
+        robot. Check code permits an error
+        state and try again.
+        """;
 
+  /** Constructs a {@code MechanicalRuntimeException} with the default message. */
   public MechanicalRuntimeException() {
     super(DEFAULT_MSG);
   }
 
+  /**
+   * Constructs a {@code MechanicalRuntimeException} with a simple string message. No additional
+   * formatting is allowed.
+   *
+   * @param errorMessage String holding the error message.
+   */
   public MechanicalRuntimeException(String errorMessage) {
     super(errorMessage);
   }
 
+  /**
+   * Constructs a {@code MechanicalRuntimeException} with a formatted string message. Use standard
+   * format string parameters to format an error message. A newline character is automatically
+   * added.
+   *
+   * @param errorMessage Format string holding the error message.
+   * @param robotStateFields Variadic list of arguments that hydrates the format string.
+   */
   public MechanicalRuntimeException(String errorMessageFmtString, Object... robotStateFields) {
     super(String.format(errorMessageFmtString, robotStateFields) + "\n");
   }
