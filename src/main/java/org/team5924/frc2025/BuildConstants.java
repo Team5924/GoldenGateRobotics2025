e4dd3bb3b5aad27379fd14163c2357a47f489a1a
Jack Doherty <jack.doherty@oracle.com>
2025-01-20 13:59:51 -0800

    removed errant space in license header file column 1

--- src/main/java/org/team5924/frc2025/BuildConstants.java
+++ src/main/java/org/team5924/frc2025/BuildConstants.java
@@ -2,17 +2,17 @@
  * BuildConstants.java
  */
 
- /* 
-  * Copyright (C) 2024-2025 Team 5924 - Golden Gate Robotics and/or its affiliates.
-  *
-  * This file, and the associated project, are offered under the GNU General
-  * Public License v3.0. A copy of this license can be found in LICENSE.md
-  * at the root of this project.
-  *
-  * If this file has been seperated from the original project, you should have
-  * received a copy of the GNU General Public License along with it.
-  * If you did not, see <https://www.gnu.org/licenses>.
-  */
+/* 
+ * Copyright (C) 2024-2025 Team 5924 - Golden Gate Robotics and/or its affiliates.
+ *
+ * This file, and the associated project, are offered under the GNU General
+ * Public License v3.0. A copy of this license can be found in LICENSE.md
+ * at the root of this project.
+ *
+ * If this file has been seperated from the original project, you should have
+ * received a copy of the GNU General Public License along with it.
+ * If you did not, see <https://www.gnu.org/licenses>.
+ */
 
 package org.team5924.frc2025;
 
@@ -21,12 +21,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "GoldenGateRobotics2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 8;
-  public static final String GIT_SHA = "af559565f6c60ae957a4d0113a88dd52ddbc5e22";
-  public static final String GIT_DATE = "2025-01-14 03:38:12 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-16 22:26:24 EST";
-  public static final long BUILD_UNIX_TIME = 1737084384736L;
+  public static final int GIT_REVISION = 9;
+  public static final String GIT_SHA = "d66c6ccc27a4eeddac14c357fac10f788335678d";
+  public static final String GIT_DATE = "2025-01-16 23:45:57 EST";
+  public static final String GIT_BRANCH = "17-fix-license-header-file";
+  public static final String BUILD_DATE = "2025-01-20 16:59:20 EST";
+  public static final long BUILD_UNIX_TIME = 1737410360454L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
