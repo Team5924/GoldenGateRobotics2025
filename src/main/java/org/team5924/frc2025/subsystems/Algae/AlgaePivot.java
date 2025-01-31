df3325825be95f5c88ba3abc35fdbb651b5c53ba
Newton Wong <newtonemailemail@gmail.com>
2025-01-30 20:35:11 -0800

    initial commit

---
+++ src/main/java/org/team5924/frc2025/subsystems/Algae/AlgaePivot.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.team5924.frc2025.subsystems.Algae;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class AlgaePivot extends SubsystemBase {
+  /** Creates a new AlgaePivot. */
+  public AlgaePivot() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
