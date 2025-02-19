d5589c53f03acc52cf4e3784e6afa2f16c81d9d0
Newton Wong <newtonemailemail@gmail.com>
2025-02-19 14:24:24 -0800

    initial commit

---
+++ src/main/java/org/team5924/frc2025/subsystems/rollers/Algae/AlgaeRollerIO.java
@@ -0,0 +1,38 @@
+/*
+ * CoralInAndOutIO.java
+ */
+
+/* 
+ * Copyright (C) 2024-2025 Team 5924 - Golden Gate Robotics and/or its affiliates.
+ *
+ * This file, and the associated project, are offered under the GNU General
+ * Public License v3.0. A copy of this license can be found in LICENSE.md
+ * at the root of this project.
+ *
+ * If this file has been separated from the original project, you should have
+ * received a copy of the GNU General Public License along with it.
+ * If you did not, see <https://www.gnu.org/licenses>.
+ */
+
+ package org.team5924.frc2025.subsystems.rollers.Algae;
+
+ import org.littletonrobotics.junction.AutoLog;
+ import org.team5924.frc2025.subsystems.rollers.GenericRollerSystemIO;
+import org.team5924.frc2025.subsystems.rollers.GenericRollerSystemIO.GenericRollerSystemIOInputs;
+import org.team5924.frc2025.util.LaserCAN_Measurement;
+ 
+ public interface AlgaeRollerIO extends GenericRollerSystemIO {
+ 
+   @AutoLog
+   abstract class AlgaeRollerInputs extends GenericRollerSystemIOInputs {
+     // Algae LaserCAN
+     public LaserCAN_Measurement algaeLCMeasurement = new LaserCAN_Measurement();
+     public boolean algaeLCConnected = true;
+ 
+  
+   }
+ 
+   /** Run roller at volts */
+   default void runVolts(double voltsIntakeAndShooter, double voltsHandoff) {}
+ }
+ 
---
+++ src/main/java/org/team5924/frc2025/subsystems/rollers/Algae/AlgaeRollerKrakenFOC.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package org.team5924.frc2025.subsystems.rollers.Algae;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class AlgaeRollerKrakenFOC extends SubsystemBase {
+  /** Creates a new AlgaeRollerKrakenFOC. */
+  public AlgaeRollerKrakenFOC() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
