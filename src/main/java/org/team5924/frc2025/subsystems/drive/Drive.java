90eb15790cf5b16f417a0d38facd6ce118563c59
Jack Doherty <jack.doherty@oracle.com>
2025-02-21 02:01:07 -0800

    enabled motion magic for drive, added 254 setpoint gen

--- src/main/java/org/team5924/frc2025/subsystems/drive/Drive.java
+++ src/main/java/org/team5924/frc2025/subsystems/drive/Drive.java
@@ -16,8 +16,17 @@
 
 package org.team5924.frc2025.subsystems.drive;
 
-import static edu.wpi.first.units.Units.MetersPerSecond;
-import static edu.wpi.first.units.Units.Volts;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.Logger;
+import org.team5924.frc2025.Constants;
+import org.team5924.frc2025.Constants.Mode;
+import org.team5924.frc2025.generated.TunerConstants;
+import org.team5924.frc2025.util.LocalADStarAK;
+import org.team5924.frc2025.util.swerve.SwerveSetpoint;
+import org.team5924.frc2025.util.swerve.SwerveSetpointGenerator;
 
 import com.ctre.phoenix6.CANBus;
 import com.pathplanner.lib.auto.AutoBuilder;
@@ -27,6 +36,7 @@ import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 import com.pathplanner.lib.pathfinding.Pathfinding;
 import com.pathplanner.lib.util.PathPlannerLogging;
+
 import edu.wpi.first.hal.FRCNetComm.tInstances;
 import edu.wpi.first.hal.FRCNetComm.tResourceType;
 import edu.wpi.first.hal.HAL;
@@ -43,6 +53,8 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.system.plant.DCMotor;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.Volts;
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -50,16 +62,6 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-import org.littletonrobotics.junction.AutoLogOutput;
-import org.littletonrobotics.junction.Logger;
-import org.team5924.frc2025.Constants;
-import org.team5924.frc2025.Constants.Mode;
-import org.team5924.frc2025.generated.TunerConstants;
-import org.team5924.frc2025.util.LocalADStarAK;
-import org.team5924.frc2025.util.swerve.SwerveSetpoint;
-import org.team5924.frc2025.util.swerve.SwerveSetpointGenerator;
 
 public class Drive extends SubsystemBase {
   // TunerConstants doesn't include these constants, so they are declared locally
@@ -244,7 +246,7 @@ public class Drive extends SubsystemBase {
             discreteSpeeds,
             Constants.LOOP_PERIODIC_SECONDS);
     SwerveModuleState[] setpointStates = previousSetpoint.moduleStates();
-    SwerveDriveKinematics.desaturateWheelSpeeds(setpointStates, TunerConstants.kSpeedAt12Volts);
+    // SwerveDriveKinematics.desaturateWheelSpeeds(setpointStates, TunerConstants.kSpeedAt12Volts);
 
     // Log unoptimized setpoints and setpoint speeds
     Logger.recordOutput("SwerveStates/Setpoints", setpointStates);
