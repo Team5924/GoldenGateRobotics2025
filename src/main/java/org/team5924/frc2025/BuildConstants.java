0646d4a456e60c1e84d6f92ed40adafc667d381c
Newton Wong <newtonemailemail@gmail.com>
2025-03-01 16:47:52 -0800

    debug wip new constants

--- src/main/java/org/team5924/frc2025/BuildConstants.java
+++ src/main/java/org/team5924/frc2025/BuildConstants.java
@@ -21,12 +21,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "GoldenGateRobotics2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 165;
-  public static final String GIT_SHA = "cd89469357d5ff01321dc96056287df09b1c1d2a";
-  public static final String GIT_DATE = "2025-02-28 19:27:50 EST";
-  public static final String GIT_BRANCH = "54-elastic-dashboard";
-  public static final String BUILD_DATE = "2025-02-28 19:55:34 EST";
-  public static final long BUILD_UNIX_TIME = 1740790534287L;
+  public static final int GIT_REVISION = 205;
+  public static final String GIT_SHA = "882e633631c04f95c0d89cabd8925557fe789ff7";
+  public static final String GIT_DATE = "2025-03-01 16:41:44 EST";
+  public static final String GIT_BRANCH = "swerve-fix-and-auto-fix";
+  public static final String BUILD_DATE = "2025-03-01 19:42:45 EST";
+  public static final long BUILD_UNIX_TIME = 1740876165210L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
