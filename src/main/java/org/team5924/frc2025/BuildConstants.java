0d8bc2c5c711c4a98c66dbffa5cda55138377a6f
Newton Wong <newtonemailemail@gmail.com>
2025-03-22 15:41:48 -0700

    retune shooting voltages and auto align

--- src/main/java/org/team5924/frc2025/BuildConstants.java
+++ src/main/java/org/team5924/frc2025/BuildConstants.java
@@ -21,12 +21,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "GoldenGateRobotics2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 220;
-  public static final String GIT_SHA = "15a96fa16e202388b8d9841bca0723cebfa7f561";
-  public static final String GIT_DATE = "2025-03-22 13:55:08 EDT";
+  public static final int GIT_REVISION = 221;
+  public static final String GIT_SHA = "188593ae262759d269f88e496d8645840c48ac6c";
+  public static final String GIT_DATE = "2025-03-22 15:13:56 EDT";
   public static final String GIT_BRANCH = "sfr-autos";
-  public static final String BUILD_DATE = "2025-03-22 15:08:03 EDT";
-  public static final long BUILD_UNIX_TIME = 1742670483015L;
+  public static final String BUILD_DATE = "2025-03-22 17:50:42 EDT";
+  public static final long BUILD_UNIX_TIME = 1742680242165L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
