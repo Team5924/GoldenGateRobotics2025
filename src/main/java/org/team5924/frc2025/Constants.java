6f7fad9b0f3268ca08c3a0e1bb45378ce67d22dc
D0LPH1NB01 <gsuite649@gmail.com>
2025-04-04 15:33:57 -0700

    set tuning_mode to false

--- src/main/java/org/team5924/frc2025/Constants.java
+++ src/main/java/org/team5924/frc2025/Constants.java
@@ -53,7 +53,7 @@ public final class Constants {
     REPLAY
   }
 
-  public static final boolean TUNING_MODE = true;
+  public static final boolean TUNING_MODE = false;
   public static final boolean ALLOW_ASSERTS = false;
 
   /* Field */
