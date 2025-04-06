335559c04de83aa24f7aa7c1d4e54041da73cda5
Newton Wong <142123723+SoggieBoi@users.noreply.github.com>
2025-04-05 17:00:04 -0700

    Auto align branching fix (#83)
    * builds, needs testing
    
    * fix build issues and merge conflicts
    
    * set tuning_mode to false
    
    * fix autos
    
    * fix autos for the second time
    
    * reef center change
    
    * push autos for match
    
    * semi-final tuning
    
    * finalize offset constants
    
    * autos and tuning works
    
    * fix autos
    
    ---------
    
    Co-authored-by: marisa07 <mhoosen2@gmail.com>
    Co-authored-by: bluesupergiant <135282744+bluesupergiant@users.noreply.github.com>
    Co-authored-by: D0LPH1NB01 <gsuite649@gmail.com>

--- src/main/java/org/team5924/frc2025/BuildConstants.java
+++ src/main/java/org/team5924/frc2025/BuildConstants.java
@@ -21,12 +21,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "GoldenGateRobotics2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 261;
-  public static final String GIT_SHA = "ee01ef8e151ba79487087c195fac7c241fdb8556";
-  public static final String GIT_DATE = "2025-04-04 12:08:29 EDT";
-  public static final String GIT_BRANCH = "gamma_merge_test_branch";
-  public static final String BUILD_DATE = "2025-04-04 15:16:16 EDT";
-  public static final long BUILD_UNIX_TIME = 1743794176123L;
+  public static final int GIT_REVISION = 284;
+  public static final String GIT_SHA = "95991a1e70b64e70023f2056ce1dd0e9a78f855f";
+  public static final String GIT_DATE = "2025-04-05 18:55:27 EDT";
+  public static final String GIT_BRANCH = "auto_align_branching_fix";
+  public static final String BUILD_DATE = "2025-04-05 19:18:51 EDT";
+  public static final long BUILD_UNIX_TIME = 1743895131176L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/org/team5924/frc2025/RobotContainer.java
+++ src/main/java/org/team5924/frc2025/RobotContainer.java
@@ -20,6 +20,7 @@ import static edu.wpi.first.units.Units.Seconds;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.events.EventTrigger;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.GenericHID;
@@ -32,8 +33,6 @@ import edu.wpi.first.wpilibj2.command.DeferredCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import java.util.Set;
-import org.team5924.frc2025.commands.coralInAndOut.RunIntake;
-import org.team5924.frc2025.commands.coralInAndOut.RunShooter;
 import org.team5924.frc2025.commands.coralInAndOut.TeleopShoot;
 import org.team5924.frc2025.commands.drive.DriveCommands;
 import org.team5924.frc2025.commands.elevator.RunElevator;
@@ -52,6 +51,7 @@ import org.team5924.frc2025.subsystems.elevator.Elevator;
 import org.team5924.frc2025.subsystems.elevator.ElevatorIO;
 import org.team5924.frc2025.subsystems.elevator.ElevatorIOTalonFXGamma;
 import org.team5924.frc2025.subsystems.rollers.CoralInAndOut.CoralInAndOut;
+import org.team5924.frc2025.subsystems.rollers.CoralInAndOut.CoralInAndOut.CoralState;
 import org.team5924.frc2025.subsystems.rollers.CoralInAndOut.CoralInAndOutIO;
 import org.team5924.frc2025.subsystems.rollers.CoralInAndOut.CoralInAndOutIOKrakenFOC;
 import org.team5924.frc2025.subsystems.rollers.CoralInAndOut.CoralInAndOutIOSim;
@@ -130,8 +130,13 @@ public class RobotContainer {
         break;
     }
 
-    NamedCommands.registerCommand("Run Shooter", new RunShooter(coralInAndOut));
-    NamedCommands.registerCommand("Run Intake", new RunIntake(coralInAndOut));
+    NamedCommands.registerCommand(
+        "Run Shooter", Commands.runOnce(() -> coralInAndOut.setGoalState(CoralState.SHOOTING_L4)));
+    NamedCommands.registerCommand(
+        "Run Intake", Commands.runOnce(() -> coralInAndOut.setGoalState(CoralState.INTAKING)));
+    NamedCommands.registerCommand(
+        "Coral In Intake",
+        Commands.runOnce(() -> coralInAndOut.setGoalState(CoralState.STORED_CORAL_IN_INTAKE)));
     NamedCommands.registerCommand(
         "Elevator Height L4",
         Commands.runOnce(() -> elevator.setGoalState(Elevator.ElevatorState.L4)));
@@ -141,6 +146,16 @@ public class RobotContainer {
     NamedCommands.registerCommand(
         "Elevator Height Intake",
         Commands.runOnce(() -> elevator.setGoalState(Elevator.ElevatorState.INTAKE)));
+    NamedCommands.registerCommand(
+        "Elevator Height Zero",
+        Commands.runOnce(() -> elevator.setGoalState(Elevator.ElevatorState.STOW)));
+
+    new EventTrigger("Elevator Height L4 Trigger")
+        .onTrue(Commands.runOnce(() -> elevator.setGoalState(Elevator.ElevatorState.L4)));
+    new EventTrigger("Elevator Height Intake Trigger")
+        .onTrue(Commands.runOnce(() -> elevator.setGoalState(Elevator.ElevatorState.INTAKE)));
+    new EventTrigger("Elevator Height Zero Trigger")
+        .onTrue(Commands.runOnce(() -> elevator.setGoalState(Elevator.ElevatorState.STOW)));
 
     // Set up auto routines
     boolean isCompetition = true;
@@ -287,6 +302,7 @@ public class RobotContainer {
         .onTrue(Commands.runOnce(() -> elevator.setGoalState(Elevator.ElevatorState.INTAKE)));
 
     // Vision
+    // vision.setDefaultCommand(new RunVisionPoseEstimation(drive, vision).ignoringDisable(true));
     // vision.setDefaultCommand(new RunVisionPoseEstimation(drive, vision).ignoringDisable(true));
 
     // Climber
