871c7ab6f027745c401984afed7478d5bfb16d97
Newton Wong <142123723+SoggieBoi@users.noreply.github.com>
2025-04-06 07:04:02 -0700

    retune L2 elevator (#84)

--- src/main/java/org/team5924/frc2025/BuildConstants.java
+++ src/main/java/org/team5924/frc2025/BuildConstants.java
@@ -21,12 +21,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "GoldenGateRobotics2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 284;
-  public static final String GIT_SHA = "95991a1e70b64e70023f2056ce1dd0e9a78f855f";
-  public static final String GIT_DATE = "2025-04-05 18:55:27 EDT";
-  public static final String GIT_BRANCH = "auto_align_branching_fix";
-  public static final String BUILD_DATE = "2025-04-05 19:18:51 EDT";
-  public static final long BUILD_UNIX_TIME = 1743895131176L;
+  public static final int GIT_REVISION = 270;
+  public static final String GIT_SHA = "335559c04de83aa24f7aa7c1d4e54041da73cda5";
+  public static final String GIT_DATE = "2025-04-05 20:00:04 EDT";
+  public static final String GIT_BRANCH = "elevator-hotfix";
+  public static final String BUILD_DATE = "2025-04-05 20:25:07 EDT";
+  public static final long BUILD_UNIX_TIME = 1743899107859L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
